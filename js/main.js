e4ab0139bce7f6dd4cf03b2dd53d50fa5eb1be75
jayfallon <jayfallon@gmail.com>
2017-12-28 21:59:45 +0700

    refactored index draw and with it, yes, the view update without reloading

--- js/main.js
+++ js/main.js
@@ -1,156 +1,175 @@
 (function(){
-  // retrieve old Promotions and populate array, otherwise create empty
-  var oldPromotions = JSON.parse(localStorage.getItem('PromotionsArray')) || [];
-  console.log(oldPromotions);
 
-  if (oldPromotions.length == 0) {
-    oldPromotions = [{"prNumber":"itdz5rrgcxoxo","prName":"All Argentine Reds","prCode":"ARG18","prStatus":"active","prAmount":"10","prType":"percent","minPurch":"100","saleItems":"included","perStart":"12/26/17","perStop":"12/27/17","priceMin":"100","priceMax":"1000","filType":"wine","filStyle":"red","filRegion":"argentina","prMessage":"Save 10% on Argentinian red wine from 12/28/17 to 1/4/18 when you spend $100 or more."},{"prNumber":"itdz5rrgctest","prName":"Spanish Rioja Special","prCode":"RIO17","prStatus":"inactive","prAmount":"12","prType":"percent","minPurch":"25","saleItems":"included","perStart":"12/26/17","perStop":"12/27/17","priceMin":"10","priceMax":"100","filType":"wine","filStyle":"red","filRegion":"spain","prMessage":"Save 12% on Spanish red wine from 12/28/17 to 1/4/18 when you spend $25 or more."}];
-    localStorage.setItem('PromotionsArray', JSON.stringify(oldPromotions));
+  function setStatus() {
+    var statusToggle = document.getElementsByClassName("status-toggle");
+    for (var i = 0; i < statusToggle.length; i++) {
+        statusToggle[i].addEventListener('click', statusChange);
+    }
   }
 
-  var contentBg = document.getElementById('content-bg');
-  var element = "";
-
-  function writeRowToPage(dataObject, element) {
-      //prName
-      prName = document.createElement('div');
-      prName.setAttribute('class', 'promo-data name');
-      prNameSpan = document.createElement('span');
-      prNameLink = document.createElement('a');
-      prNameLink.setAttribute('href', 'view.html?' + dataObject.prNumber);
-      prNameSpan.append(prNameLink);
-      prNameData = document.createTextNode(dataObject.prName);
-      prNameLink.appendChild(prNameData);
-      prName.appendChild(prNameSpan);
-      element.appendChild(prName);
-      //prCode
-      prCode = document.createElement('div');
-      prCode.setAttribute('class', 'promo-data code');
-      prCodeSpan = document.createElement('span');
-      prCodeData = document.createTextNode(dataObject.prCode);
-      prCodeSpan.append(prCodeData);
-      prCode.appendChild(prCodeSpan);
-      element.appendChild(prCode);
-      //prAmount
-      prAmount = document.createElement('div');
-      prAmount.setAttribute('class', 'promo-data disc');
-      prAmountSpan = document.createElement('span');
-      if (dataObject.prType == 'amount') {
-        prAmountData = document.createTextNode('$'+dataObject.prAmount);
-        prAmountSpan.append(prAmountData);
-      } else if (dataObject.prType == 'percent') {
-        prAmountData = document.createTextNode(dataObject.prAmount+'%');
-        prAmountSpan.append(prAmountData);
-      } else if (dataObject.prType == 'frees') {
-        prAmountData = document.createTextNode('Free Shipping');
-        prAmountSpan.append(prAmountData);
-      } else if (dataObject.prType == 'freed') {
-        prAmountData = document.createTextNode('Free Delivery');
-        prAmountSpan.append(prAmountData);
-      }
-      prAmount.appendChild(prAmountSpan);
-      element.appendChild(prAmount);
-      //period
-      period = document.createElement('div');
-      period.setAttribute('class', 'promo-data period');
-      if (dataObject.perStart && dataObject.perStop) {
-        periodSpan = document.createElement('span');
-        periodData = document.createTextNode(dataObject.perStart + ' - ' + dataObject.perStop);
-        periodSpan.append(periodData);
-        period.appendChild(periodSpan);
-      }
-      element.appendChild(period);
-      //minimum
-      minimum = document.createElement('div');
-      minimum.setAttribute('class', 'promo-data min');
-      if (dataObject.priceMin) {
-        minimumSpan = document.createElement('span');
-        minimumData = document.createTextNode('$'+dataObject.priceMin);
-        minimumSpan.append(minimumData);
-        minimum.appendChild(minimumSpan);
-      }
-      element.appendChild(minimum);
-      //prStatus
-      prStatus = document.createElement('div');
-      prStatus.setAttribute('class', 'promo-data status');
-      prStatusSpan = document.createElement('span');
-      prStatusData = document.createTextNode(dataObject.prStatus);
-      prStatusSpan.append(prStatusData);
-      prStatus.appendChild(prStatusSpan);
-      element.appendChild(prStatus);
-      //statusDis
-      statusDis = document.createElement('div');
-      statusDis.setAttribute('class', 'promo-data click');
-      statusDisSpan = document.createElement('span');
-      statusDisImage = document.createElement('img');
-      statusDisImage.setAttribute('class', 'status-toggle');
-      statusDisImage.setAttribute('data', [i]);
-      if (dataObject.prStatus === 'active') {
-        statusDisImage.setAttribute('src', '/img/state-active.svg');
-      } else {
-        statusDisImage.setAttribute('src', '/img/state-inactive.svg');
-      }
-      statusDisImage.setAttribute('alt', 'Promotion Status');
-      statusDisSpan.append(statusDisImage);
-      statusDis.appendChild(statusDisSpan);
-      element.appendChild(statusDis);
-      if (dataObject.prMessage.length) {
-        //pmsgHead
-        pmsgHead = document.createElement('div');
-        pmsgHead.setAttribute('class', 'promo-heading message');
-        pmsgHeadSpan = document.createElement('span');
-        pmsgHeadData = document.createTextNode('Promotional Message');
-        pmsgHeadSpan.append(pmsgHeadData);
-        pmsgHead.appendChild(pmsgHeadSpan);
-        element.appendChild(pmsgHead);
-        //prMessage
-        prMessage = document.createElement('div');
-        prMessage.setAttribute('class', 'promo-data message');
-        prMessageSpan = document.createElement('span');
-        prMessageData = document.createTextNode(dataObject.prMessage);
-        prMessageSpan.append(prMessageData);
-        prMessage.appendChild(prMessageSpan);
-        element.appendChild(prMessage);
-      }
+  function retreiveData() {
+    // retrieve old Promotions and populate array, otherwise create empty
+    oldPromotions = JSON.parse(localStorage.getItem('PromotionsArray')) || [];
+    //seed some data if empty
+    if (oldPromotions.length == 0) {
+      oldPromotions = [
+        {"prNumber":"itdz5rrgcxoxo","prName":"All Argentine Reds","prCode":"ARG18","prStatus":"active","prAmount":"10","prType":"percent","minPurch":"100","saleItems":"included","perStart":"12/26/17","perStop":"12/27/17","priceMin":"100","priceMax":"1000","filType":"wine","filStyle":"red","filRegion":"argentina","prMessage":"Save 10% on Argentinian red wine from 12/28/17 to 1/4/18 when you spend $100 or more."},
+        {"prNumber":"itdz5rrgctest","prName":"Spanish Rioja Special","prCode":"RIO17","prStatus":"inactive","prAmount":"12","prType":"percent","minPurch":"25","saleItems":"included","perStart":"12/26/17","perStop":"12/27/17","priceMin":"10","priceMax":"100","filType":"wine","filStyle":"red","filRegion":"spain","prMessage":"Save 12% on Spanish red wine from 12/28/17 to 1/4/18 when you spend $25 or more."}
+        ];
+      localStorage.setItem('PromotionsArray', JSON.stringify(oldPromotions));
+    }
   }
 
-  //populate promotions list from old promotions
-  // if old promotions exists
-  if (oldPromotions != null) {
-      for (var i = 0; i < oldPromotions.length; i++) {
-          for (prStatus in oldPromotions[i]) {
-            if (oldPromotions[i][prStatus] == 'active') {
-              myStatus = document.getElementById("status-active");
-              myStatus.style.display = 'none';
-              element = document.getElementById("promotions-grid-active");
-              element.style.visibility = 'visible';
-            } else if (oldPromotions[i][prStatus] == 'inactive') {
-              myStatus = document.getElementById("status-inactive");
-              myStatus.style.display = 'none';
-              element = document.getElementById("promotions-grid-inactive");
-              element.style.visibility = 'visible';
-            }
-          }
-          writeRowToPage(oldPromotions[i],element);
-      }
+  function writeToBg(item, index) {
+    var activeStatus = document.getElementById('promotions-grid-active');
+    var inactiveStatus = document.getElementById('promotions-grid-inactive');
+    var frag = document.createDocumentFragment();
+
+    //prName
+    prName = document.createElement('div');
+    prName.setAttribute('class', 'promo-data name');
+    prNameSpan = document.createElement('span');
+    prNameLink = document.createElement('a');
+    prNameLink.setAttribute('href', 'view.html?' + item.prNumber);
+    prNameSpan.append(prNameLink);
+    prNameData = document.createTextNode(item.prName);
+    prNameLink.appendChild(prNameData);
+    prName.appendChild(prNameSpan);
+    frag.appendChild(prName);
+    //prCode
+    prCode = document.createElement('div');
+    prCode.setAttribute('class', 'promo-data code');
+    prCodeSpan = document.createElement('span');
+    prCodeData = document.createTextNode(item.prCode);
+    prCodeSpan.append(prCodeData);
+    prCode.appendChild(prCodeSpan);
+    frag.appendChild(prCode);
+    //prAmount
+    prAmount = document.createElement('div');
+    prAmount.setAttribute('class', 'promo-data disc');
+    prAmountSpan = document.createElement('span');
+    if (item.prType == 'amount') {
+      prAmountData = document.createTextNode('$'+item.prAmount);
+      prAmountSpan.append(prAmountData);
+    } else if (item.prType == 'percent') {
+      prAmountData = document.createTextNode(item.prAmount+'%');
+      prAmountSpan.append(prAmountData);
+    } else if (item.prType == 'frees') {
+      prAmountData = document.createTextNode('Free Shipping');
+      prAmountSpan.append(prAmountData);
+    } else if (item.prType == 'freed') {
+      prAmountData = document.createTextNode('Free Delivery');
+      prAmountSpan.append(prAmountData);
+    }
+    prAmount.appendChild(prAmountSpan);
+    frag.appendChild(prAmount);
+    //period
+    period = document.createElement('div');
+    period.setAttribute('class', 'promo-data period');
+    if (item.perStart && item.perStop) {
+      periodSpan = document.createElement('span');
+      periodData = document.createTextNode(item.perStart + ' - ' + item.perStop);
+      periodSpan.append(periodData);
+      period.appendChild(periodSpan);
+    }
+    frag.appendChild(period);
+    //minimum
+    minimum = document.createElement('div');
+    minimum.setAttribute('class', 'promo-data min');
+    if (item.priceMin) {
+      minimumSpan = document.createElement('span');
+      minimumData = document.createTextNode('$'+item.priceMin);
+      minimumSpan.append(minimumData);
+      minimum.appendChild(minimumSpan);
+    }
+    frag.appendChild(minimum);
+    //prStatus
+    prStatus = document.createElement('div');
+    prStatus.setAttribute('class', 'promo-data status');
+    prStatusSpan = document.createElement('span');
+    prStatusData = document.createTextNode(item.prStatus);
+    prStatusSpan.append(prStatusData);
+    prStatus.appendChild(prStatusSpan);
+    frag.appendChild(prStatus);
+    //statusDis
+    statusDis = document.createElement('div');
+    statusDis.setAttribute('class', 'promo-data click');
+    statusDisSpan = document.createElement('span');
+    statusDisImage = document.createElement('img');
+    statusDisImage.setAttribute('class', 'status-toggle');
+    statusDisImage.setAttribute('data', index);
+    if (item.prStatus === 'active') {
+      statusDisImage.setAttribute('src', '/img/state-active.svg');
+    } else {
+      statusDisImage.setAttribute('src', '/img/state-inactive.svg');
+    }
+    statusDisImage.setAttribute('alt', 'Promotion Status');
+    statusDisSpan.append(statusDisImage);
+    statusDis.appendChild(statusDisSpan);
+    frag.appendChild(statusDis);
+    if (item.prMessage.length) {
+      //pmsgHead
+      pmsgHead = document.createElement('div');
+      pmsgHead.setAttribute('class', 'promo-heading message');
+      pmsgHeadSpan = document.createElement('span');
+      pmsgHeadData = document.createTextNode('Promotional Message');
+      pmsgHeadSpan.append(pmsgHeadData);
+      pmsgHead.appendChild(pmsgHeadSpan);
+      frag.appendChild(pmsgHead);
+      //prMessage
+      prMessage = document.createElement('div');
+      prMessage.setAttribute('class', 'promo-data message');
+      prMessageSpan = document.createElement('span');
+      prMessageData = document.createTextNode(item.prMessage);
+      prMessageSpan.append(prMessageData);
+      prMessage.appendChild(prMessageSpan);
+      frag.appendChild(prMessage);
+    }
+    if (item.prStatus == 'active') {
+      myActiveStatus = document.getElementById("status-active");
+      myActiveStatus.style.display = 'none';
+      myActives = document.getElementById("promotions-grid-active");
+      myActives.style.visibility = 'visible';
+      activeStatus.appendChild(frag);
+    } else if (item.prStatus == 'inactive') {
+      myInactiveStatus = document.getElementById("status-inactive");
+      myInactiveStatus.style.display = 'none';
+      myInactives = document.getElementById("promotions-grid-inactive");
+      myInactives.style.visibility = 'visible';
+      inactiveStatus.appendChild(frag);
+    }
+    setStatus();
   }
+
+  function populateContent(contentArray) {
+    contentArray.map(writeToBg);
+  }
+  retreiveData();
+  populateContent(oldPromotions);
 
   function statusChange() {
+    retreiveData();
     var status = this.getAttribute("data");
+    // console.log(status);
     if (oldPromotions[status].prStatus == "active") {
       oldPromotions[status].prStatus = "inactive";
     } else if (oldPromotions[status].prStatus == "inactive") {
       oldPromotions[status].prStatus = "active";
     }
     localStorage.setItem('PromotionsArray', JSON.stringify(oldPromotions));
-    location.assign("/");
+    // location.assign("/");
+    myActiveNode = document.getElementById("promotions-grid-active");
+    while (myActiveNode.firstChild) {
+        myActiveNode.removeChild(myActiveNode.firstChild);
+    }
+    myInactiveNode = document.getElementById("promotions-grid-inactive");
+    while (myInactiveNode.firstChild) {
+        myInactiveNode.removeChild(myInactiveNode.firstChild);
+    }
+    myActiveStatus = document.getElementById("status-active");
+    myActiveStatus.style.display = 'inline';
+    myInactiveStatus = document.getElementById("status-inactive");
+    myInactiveStatus.style.display = 'inline';
+    populateContent(oldPromotions);
   }
-
-  var statusToggle = document.getElementsByClassName("status-toggle");
-
-  for (var i = 0; i < statusToggle.length; i++) {
-      statusToggle[i].addEventListener('click', statusChange);
-  }
-
-  // console.log(statusToggle);
 })();
